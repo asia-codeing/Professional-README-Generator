93b940873a843d57e26fceccd01bdbd0c514f780
Asia Alnahi <asia.alius@gmail.com>
2021-03-30 20:28:02 -0700

    Created User input function

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,53 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(userInput) {
+  return `# ${userInput.title}
+  
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-${userInput.licensing}-brightgreen)](https://shields.io)
+
+  ## Description:
+  ${userInput.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [Contributors](#contributors)
+  * [License](#license)
+  * [Contact-info](#contact-info)
+  
+  ## Installation
+  ${userInput.installation}
+
+  ## Usage
+  ${userInput.usage}
+  
+  ## Credits
+  ${userInput.credits}
+
+  ## Contributors
+  ${userInput.contributors}
+
+  ## License
+  ${userInput.licensing}
+
+  ## Contact Info
+  * Portfolio: ${userInput.portfolio}
+  * Email: ${userInput.email}
+
+`;
+}
+
+module.exports = generateMarkdown;
