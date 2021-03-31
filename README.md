a252b53d9158744b78bd0a1641218dc0be0ba4d7
Asia Alnahi <asia.alius@gmail.com>
2021-03-30 21:06:40 -0700

    Added some Questions and test the init function

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Professional-README-Generator
+# 
+  
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-undefined-brightgreen)](https://shields.io)
+
+  ## Description:
+  
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [Contributors](#contributors)
+  * [License](#license)
+  * [Contact-info](#contact-info)
+  
+  ## Installation
+  
+
+  ## Usage
+  undefined
+  
+  ## Credits
+  undefined
+
+  ## Contributors
+  undefined
+
+  ## License
+  undefined
+
+  ## Contact Info
+  * Portfolio: undefined
+  * Email: undefined
+
--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
+ 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+//---Project Title
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the project name? (Required)',
+    validate: function(input) {
+        if (input.length = 0) {
+            return console.log('You need to enter a title to continue!');
+        } else {
+            return true;
+        }
+    }  
+},
+//---Description
+{
+
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of the project (Required)',
+    validate: function(input) {
+        if (input.length = 0) {
+            return console.log('You need to enter a Description to continue!');
+        } else {
+            return true;
+        }
+    }  
+},
+//----- Installation
+{
+
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install the project? (Required)',
+    validate: function(input) {
+        if (input.length = 0) {
+            return console.log('You need to provide installation info to continue!');
+        } else {
+            return true;
+        }
+    }  
+},
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) =>
+     error ? console.log(error) : console.log('Success! Information transferred to the README!'));
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();
